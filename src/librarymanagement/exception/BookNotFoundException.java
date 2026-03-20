87b96482ec3dec7a9daffdf4972a535f07320002
DuperXD <planeman624@gmail.com>
2026-03-20 11:30:48 +0530

    Update BookNotFoundException.java

--- src/librarymanagement/exception/BookNotFoundException.java
+++ src/librarymanagement/exception/BookNotFoundException.java
@@ -1,1 +1,8 @@
+package librarymanagement.exception;
 
+public class BookNotFoundException extends Exception {
+
+    public BookNotFoundException(String message) {
+        super(message);
+    }
+}
