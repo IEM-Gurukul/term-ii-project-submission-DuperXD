975258718d6e48a7b4bcfdbf86ae3dd5578004de
DuperXD <planeman624@gmail.com>
2026-03-20 11:04:07 +0530

    Update FacultyMember.java

--- src/model/FacultyMember.java
+++ src/model/FacultyMember.java
@@ -1,1 +1,13 @@
+package librarymanagement.model;
 
+public class FacultyMember extends Member {
+
+    public FacultyMember(int memberId, String name) {
+        super(memberId, name);
+    }
+
+    @Override
+    public int getBorrowLimit() {
+        return 5;   // Faculty can borrow max 5 books
+    }
+}
